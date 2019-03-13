c568ed82c2e262e2fdcd0712b6635949130cfa48
Mike <migueljrojas@gmail.com>
2019-03-13 15:51:13 -0300

    Final touches

--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -7,7 +7,7 @@ global.$ = global.jQuery = require('jquery');
 global._ = require('underscore');
 var Header = require('../_modules/header/header');
 var Slider = require('../_modules/slider/slider');
-var Slider = require('../_modules/eg-modal/eg-modal');
+var Modal = require('../_modules/eg-modal/eg-modal');
 
 $(function() {
     require('../../bower_components/bootstrap-sass/assets/javascripts/bootstrap.min');
@@ -15,4 +15,5 @@ $(function() {
 
     new Header();
     new Slider();
+    new Modal();
 });
